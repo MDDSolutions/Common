74804015259bf57eb3b59dc98b4ded579955123e
mdefehr <mdefehr@defehr.com>
2024-09-19 14:50:08 -0400

    Initial commit of MDDSQLCLR

---
+++ MDDSQLCLR/MDDSQLCLR/RegExFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Text.RegularExpressions;
+using Microsoft.SqlServer.Server;
+
+namespace MDDSQLCLR
+{
+    public class RegexFunctions
+    {
+        [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+        public static SqlBoolean RegexMatch(SqlString input, SqlString pattern)
+        {
+            if (input.IsNull || pattern.IsNull)
+                return SqlBoolean.Null;
+
+            return Regex.IsMatch(input.Value, pattern.Value, RegexOptions.IgnoreCase);
+        }
+    }
+
+}
