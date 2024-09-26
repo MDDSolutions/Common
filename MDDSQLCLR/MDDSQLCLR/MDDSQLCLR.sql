49fc6cd175c070c7ac38733ea154646ce262994f
mdefehr <mdefehr@defehr.com>
2024-09-26 16:15:16 -0400

    Added StringSimilarity

--- MDDSQLCLR/MDDSQLCLR/MDDSQLCLR.sql
+++ MDDSQLCLR/MDDSQLCLR/MDDSQLCLR.sql
@@ -15,21 +15,25 @@ IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.RegexSpli
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.RegexMatches') AND type IN (N'FN', N'IF', N'TF', N'FS', N'FT'))
     DROP FUNCTION dbo.RegexMatches;
 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.LevenshteinSimilarity') AND type IN (N'FN', N'IF', N'TF', N'FS', N'FT'))
+    DROP FUNCTION dbo.LevenshteinSimilarity;
+
 -- Drop assembly if it exists
 IF EXISTS (SELECT * FROM sys.assemblies WHERE name = N'MDDSQLCLR')
     DROP ASSEMBLY MDDSQLCLR;
 GO
 
-
+/*
 EXEC sys.sp_configure @configname = 'clr strict security', -- varchar(35)
                       @configvalue = 0  -- int
 GO
 RECONFIGURE
 GO
+*/
 
 -- Create assembly
 CREATE ASSEMBLY MDDSQLCLR
-FROM 'D:\SoftwareDeploy\MDDSQLCLR.dll'
+FROM 'D:\Temp\MDDSQLCLR.dll'
 WITH PERMISSION_SET = SAFE;
 GO
 
@@ -52,4 +56,9 @@ GO
 CREATE FUNCTION dbo.RegexMatches(@input NVARCHAR(MAX), @pattern NVARCHAR(MAX))
 RETURNS TABLE (Value NVARCHAR(MAX), MatchIndex INT, MatchLength INT)
 AS EXTERNAL NAME MDDSQLCLR.[MDDSQLCLR.RegexFunctions].RegexMatches;
+GO
+
+CREATE FUNCTION dbo.LevenshteinSimilarity(@s1 NVARCHAR(MAX), @s2 NVARCHAR(MAX))
+RETURNS FLOAT
+AS EXTERNAL NAME MDDSQLCLR.[MDDSQLCLR.StringSimilarity].LevenshteinSimilarity;
 GO
---
+++ MDDSQLCLR/MDDSQLCLR/StringSimilarity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+namespace MDDSQLCLR
+{
+    public class StringSimilarity
+    {
+        [SqlFunction(IsDeterministic = true, IsPrecise = false)]
+        public static SqlDouble LevenshteinSimilarity(SqlString s1, SqlString s2)
+        {
+            if (s1.IsNull || s2.IsNull)
+                return SqlDouble.Null;
+
+            string str1 = s1.Value;
+            string str2 = s2.Value;
+
+            int distance = LevenshteinDistance(str1, str2);
+            int maxLen = Math.Max(str1.Length, str2.Length);
+
+            if (maxLen == 0) return 1.0; // Both strings are empty
+
+            double similarity = 1.0 - (double)distance / maxLen;
+            return new SqlDouble(similarity);
+        }
+
+        private static int LevenshteinDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            for (int i = 0; i <= n; i++)
+                d[i, 0] = i;
+            for (int j = 0; j <= m; j++)
+                d[0, j] = j;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return d[n, m];
+        }
+    }
+}
